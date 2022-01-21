e2da9a7589019b035ddd9771a524b827e1306623
Bruno Martins <bruno.eletr@gmail.com>
2022-01-20 22:57:49 -0300

    Atualiza projeto incluindo classe Main, banco, cliente, conta, conta corremte, conta poupança e Interface Conta

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Cliente bruno = new Cliente();
+        bruno.setNome("Bruno");
+        Conta cc = new ContaCorrente(bruno);
+
+        Conta cp = new ContaPoupanca(bruno);
+
+        cc.depositar(200.00);
+        cc.transferir(100, cp);
+        cc.transferir(2.00, cp);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+    }
+
 }
